47f2df2b0a81628693bf177873f7b6f952daa8c1
treckk00 <85478471+treckk00@users.noreply.github.com>
2022-09-02 10:51:07 -0400

    Add files via upload

---
+++ test5.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+install_phpmyadmin(){
+    echo "Installing phpMyAdmin..."
+
+    cd /var/www/pterodactyl/public
+    rm -rf phpmyadmin
+    wget https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-all-languages.zip
+    unzip phpMyAdmin-5.1.1-all-languages.zip
+    mv phpMyAdmin-5.1.1-all-languages phpmyadmin
+    rm -rf phpMyAdmin-5.1.1-all-languages.zip
+    cd /var/www/pterodactyl/public/phpmyadmin
+
+    SERVER_IP="66.23.237.244"
+    BOWFISH=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 34 | head -n 1`
+    bash -c 'cat > /var/www/pterodactyl/public/phpmyadmin/config.inc.php' <<EOF
+<?php
+/* Servers configuration */
+\$i = 0;
+/* Server: MySQL [1] */
+\$i++;
+\$cfg['Servers'][\$i]['verbose'] = 'MySQL';
+\$cfg['Servers'][\$i]['host'] = '${SERVER_IP}';
+\$cfg['Servers'][\$i]['port'] = '';
+\$cfg['Servers'][\$i]['socket'] = '';
+\$cfg['Servers'][\$i]['auth_type'] = 'cookie';
+\$cfg['Servers'][\$i]['user'] = 'root';
+\$cfg['Servers'][\$i]['password'] = '';
+/* End of servers configuration */
+\$cfg['blowfish_secret'] = '${BOWFISH}';
+\$cfg['DefaultLang'] = 'en';
+\$cfg['ServerDefault'] = 1;
+\$cfg['UploadDir'] = '';
+\$cfg['SaveDir'] = '';
+\$cfg['CaptchaLoginPublicKey'] = '6LcJcjwUAAAAAO_Xqjrtj9wWufUpYRnK6BW8lnfn';
+\$cfg['CaptchaLoginPrivateKey'] = '6LcJcjwUAAAAALOcDJqAEYKTDhwELCkzUkNDQ0J5'
+?>    
+EOF
+    echo "Installation completed."
+    if [ "$lsb_dist" =  "ubuntu" ] || [ "$lsb_dist" =  "debian" ]; then
+        chown -R www-data:www-data * /var/www/pterodactyl
+    elif [ "$lsb_dist" =  "fedora" ] || [ "$lsb_dist" =  "centos" ] || [ "$lsb_dist" =  "rhel" ]; then
+        chown -R apache:apache * /var/www/pterodactyl
+        chown -R nginx:nginx * /var/www/pterodactyl
+        semanage fcontext -a -t httpd_sys_rw_content_t "/var/www/pterodactyl/storage(/.*)?"
+        restorecon -R /var/www/pterodactyl
+    fi
+}
+
+main() {
+  install_phpmyadmin
+}
+
+main
